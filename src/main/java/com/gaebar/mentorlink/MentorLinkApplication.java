1c3f7ce50be7417834c1cda393646e2765cec6cf
gaebar <gaetanobarreca@gmail.com>
2025-07-03 17:00:13 -0700

    Initial commit: Spring Boot MentorLink project setup

---
+++ src/main/java/com/gaebar/mentorlink/MentorLinkApplication.java
@@ -0,0 +1,13 @@
+package com.gaebar.mentorlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaebar/mentorlink/MentorLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaebar.mentorlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
